4ee7799387bf971b4e855d68c90df7bee7966575
BrunoM24 <brunocostamatos.24@gmail.com>
2019-07-02 22:11:24 +0100

    Added the JokeController

---
+++ src/main/java/com/brunom24/jokesapp/controllers/JokeController.java
@@ -0,0 +1,24 @@
+package com.brunom24.jokesapp.controllers;
+
+import com.brunom24.jokesapp.services.JokeService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class JokeController {
+
+	private JokeService jokeService;
+
+	public JokeController(JokeService jokeService) {
+		this.jokeService = jokeService;
+	}
+
+	@RequestMapping({"/", ""})
+	public String showJoke(Model model){
+
+		model.addAttribute("joke", jokeService.getJoke());
+
+		return "chucknorris";
+	}
+}
